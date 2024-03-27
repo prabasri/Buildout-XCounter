9bea86ac8748a066e847704e1b2220305018e09d
Praba sri <prabasri1998@gmail.com>
2024-03-27 19:52:08 +0530

    counter finished

---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count+1);
+  }
+
+  const handleDecrement = () => {
+    setCount(count-1);
+  }
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>{`Count: ${count}`}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter;
